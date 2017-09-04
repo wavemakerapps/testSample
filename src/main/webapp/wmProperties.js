c64161db99e4b3c4aef56bb3c2510de68974f367
Tejaswi Maryala <tejaswi.maryala@wavemaker.com>
2017-09-04 09:25:49 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "testSample",
+  "homePage" : "Main",
+  "name" : "testSample",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
